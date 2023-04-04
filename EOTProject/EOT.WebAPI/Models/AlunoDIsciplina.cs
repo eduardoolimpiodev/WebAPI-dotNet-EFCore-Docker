68b348f218f97eee184541cc02cc69e85967aba6
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-04-04 15:07:33 -0300

    add: Models

---
+++ EOTProject/EOT.WebAPI/Models/AlunoDIsciplina.cs
@@ -0,0 +1,9 @@
+namespace EOT.WebAPI.Models
+{
+    public class Fornecedor
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string CNPJ { get; set; }
+    }
+}
