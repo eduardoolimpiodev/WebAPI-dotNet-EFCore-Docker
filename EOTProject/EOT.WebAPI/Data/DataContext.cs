ec73578a9a8323453959617323e59706b5cc0d54
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-04-04 19:53:59 -0300

    add: context in startup

---
+++ EOTProject/EOT.WebAPI/Data/DataContext.cs
@@ -0,0 +1,25 @@
+using EOT.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EOT.WebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Aluno> Alunos { get; set;}
+
+        public DbSet<Professor> Professores { get; set;}
+
+        public DbSet<Disciplina> Disciplinas { get; set;}
+
+        public DbSet<AlunoDisciplina> AlunosDisciplinas { get; set;}
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AlunoDisciplina>()
+                .HasKey(AD => new {AD.AlunoId, AD.DisciplinaId});
+        }
+    }
+}
