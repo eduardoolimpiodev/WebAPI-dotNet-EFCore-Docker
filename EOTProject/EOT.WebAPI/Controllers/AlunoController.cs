d301d1e719ecfdfb91afc9a7c24bda3d8882f387
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-04-04 16:29:02 -0300

    add: Controller Aluno e Professor

--- EOTProject/EOT.WebAPI/Controllers/AlunoController.cs
+++ EOTProject/EOT.WebAPI/Controllers/AlunoController.cs
@@ -1,4 +1,6 @@
 
+using System.Collections.Generic;
+using EOT.WebAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EOT.WebAPI.Controllers
@@ -7,6 +9,36 @@ namespace EOT.WebAPI.Controllers
     [Route("api/[controller]")]
     public class AlunoController : ControllerBase
     {
+        public List<Aluno> Alunos = new List<Aluno>() {
+            new Aluno() {
+                Id = 1,
+                Nome = "Eduardo",
+                Sobrenome = "Olimpio",
+                Telefone = "123456789",
 
+            },
+            new Aluno() {
+                Id = 1,
+                Nome = "Maria",
+                 Sobrenome = "Olimpio",
+                Telefone = "78456321"
+
+            },
+            new Aluno() {
+                Id = 1,
+                Nome = "Marcele",
+                 Sobrenome = "Olimpio",
+                Telefone = "123456"
+
+            },
+        };
+
+        public AlunoController() { }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Alunos);
+        }
     }
 }
